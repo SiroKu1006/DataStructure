aaf1f19d7989585d43869eda6c2d5477b99e1595
SiroKu_ <56777898+SiroKu1006@users.noreply.github.com>
2024-06-10 20:20:32 +0800

    Update sampleOut.txt and testout.txt

--- HomeWork 5/main.cpp
+++ HomeWork 5/main.cpp
@@ -3,112 +3,107 @@
 #include <string>
 #define endl '\n'
 using namespace std;
-
 class HuffmanNode {
-public:
-    HuffmanNode(char c, int f) : name(c), num(f), left(nullptr), right(nullptr) {}
-    HuffmanNode(char c, int f, HuffmanNode* l, HuffmanNode* r) : name(c), num(f), left(l), right(r) {}
+    public:
+        HuffmanNode(char c, int f) : name(c), num(f), left(nullptr), right(nullptr) {}
+        HuffmanNode(char c, int f, HuffmanNode* l, HuffmanNode* r) : name(c), num(f), left(l), right(r) {}
 
-    char name;
-    int num;
-    HuffmanNode* left;
-    HuffmanNode* right;
+        char name;
+        int num;
+        HuffmanNode* left;
+        HuffmanNode* right;
 };
-
 class MinHeap {
-private:
-    HuffmanNode** heapArray;
-    int size;
-    int capacity;
-
-    void heapify(int index) {
-        int smallest = index;
-        int left = 2 * index + 1;
-        int right = 2 * index + 2;
-
-        if (left < size && heapArray[left]->num < heapArray[smallest]->num)
-            smallest = left;
-
-        if (right < size && heapArray[right]->num < heapArray[smallest]->num)
-            smallest = right;
-
-        if (smallest != index) {
-            swap(heapArray[index], heapArray[smallest]);
-            heapify(smallest);
+    private:
+        HuffmanNode** heapArray; 
+        int size; 
+        int capacity; 
+        // 正規化
+        void heapify(int index) {
+            int smallest = index;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            //左邊右邊 (比大小)
+            if (left < size && heapArray[left]->num < heapArray[smallest]->num)
+                smallest = left;
+            if (right < size && heapArray[right]->num < heapArray[smallest]->num)
+                smallest = right;
+            //不是最小就交換
+            if (smallest != index) {
+                swap(heapArray[index], heapArray[smallest]);
+                //recursive
+                heapify(smallest);
+            }
         }
-    }
-
-public:
-    MinHeap(int capacity) : size(0), capacity(capacity) {
-        heapArray = new HuffmanNode*[capacity];
-    }
-
-    ~MinHeap() {
-        delete[] heapArray;
-    }
-
-    void insert(HuffmanNode* node) {
-        if (size == capacity) return;
-
-        int i = size++;
-        heapArray[i] = node;
-
-        while (i && heapArray[i]->num < heapArray[(i - 1) / 2]->num) {
-            swap(heapArray[i], heapArray[(i - 1) / 2]);
-            i = (i - 1) / 2;
+    public:
+        MinHeap(int capacity) : size(0), capacity(capacity) {
+            heapArray = new HuffmanNode*[capacity];
         }
-    }
-
-    HuffmanNode* extractMin() {
-        if (size == 0) return nullptr;
-        if (size == 1) return heapArray[--size];
-
-        HuffmanNode* root = heapArray[0];
-        heapArray[0] = heapArray[--size];
-        heapify(0);
-
-        return root;
-    }
-
-    int getSize() {
-        return size;
-    }
+        ~MinHeap() {
+            delete[] heapArray;
+        }
+        // insert  node to heap
+        void insert(HuffmanNode* node) {
+            if (size == capacity) return;
+            int i = size++;
+            heapArray[i] = node;
+            while (i && heapArray[i]->num < heapArray[(i - 1) / 2]->num) {
+                swap(heapArray[i], heapArray[(i - 1) / 2]);
+                i = (i - 1) / 2;
+            }
+        }
+        //return 最小頻率的節點
+        HuffmanNode* extractMin() {
+            if (size == 0) return nullptr;
+            if (size == 1) return heapArray[--size];
+            HuffmanNode* root = heapArray[0];
+            heapArray[0] = heapArray[--size];
+            heapify(0);
+            return root;
+        }
+        //return size
+        int getSize() {
+            return size;
+        }
 };
 
 class HuffmanTree {
-public:
-    HuffmanTree() : root(nullptr) {}
-    HuffmanTree(HuffmanNode* r) : root(r) {}
-    HuffmanNode* root;
-
-    void buildTree(pair<char, int> frequencies[], int n) {
-        MinHeap minHeap(n);
-
-        for (int i = 0; i < n; ++i) {
-            minHeap.insert(new HuffmanNode(frequencies[i].first, frequencies[i].second));
+    public:
+        HuffmanTree() : root(nullptr) {}
+        HuffmanTree(HuffmanNode* r) : root(r) {}
+        HuffmanNode* root;
+        //build tree
+        void buildTree(pair<char, int> frequencies[], int n) {
+            MinHeap minHeap(n);
+            for (int i = 0; i < n; ++i) {
+                //將所有節點存入Heap
+                minHeap.insert(new HuffmanNode(frequencies[i].first, frequencies[i].second));
+            }
+            while (minHeap.getSize() > 1) {
+                //取出最小的兩個節點
+                HuffmanNode* left = minHeap.extractMin();
+                HuffmanNode* right = minHeap.extractMin();
+                //合併
+                int sum = left->num + right->num;
+                //存回去
+                minHeap.insert(new HuffmanNode('\0', sum, left, right));
+            }
+            root = minHeap.extractMin();
+        }
+        // generate huffman code
+        void generateCodes(HuffmanNode* node, string code, string huffmanCode[]) {
+            //空的就return
+            if (!node) return;
+            
+            if (node->name != '\0') {
+                //將字母的存入huffmanCode
+                huffmanCode[node->name - 'A'] = code;
+            }
+            //recursive
+            generateCodes(node->left, code + "0", huffmanCode);
+            generateCodes(node->right, code + "1", huffmanCode);
         }
 
-        while (minHeap.getSize() > 1) {
-            HuffmanNode* left = minHeap.extractMin();
-            HuffmanNode* right = minHeap.extractMin();
-
-            int sum = left->num + right->num;
-            minHeap.insert(new HuffmanNode('\0', sum, left, right));
-        }
-
-        root = minHeap.extractMin();
-    }
-
-    void generateCodes(HuffmanNode* node, string code, string huffmanCode[]) {
-        if (!node) return;
-
-        if (node->name != '\0') {
-            huffmanCode[node->name - 'A'] = code;
-        }
-
-        generateCodes(node->left, code + "0", huffmanCode);
-        generateCodes(node->right, code + "1", huffmanCode);
-    }
 };
 
 int main() {
@@ -125,28 +120,23 @@ int main() {
 
     string nn;
     getline(cin, nn);
+    cout << endl;
+
     HuffmanTree huffmanTree;
     huffmanTree.buildTree(arr, n);
-
     string huffmanCode[26];
     huffmanTree.generateCodes(huffmanTree.root, "", huffmanCode);
-
-
     for (int i = 0; i < n; ++i) {
         cout << arr[i].first << " " << huffmanCode[arr[i].first - 'A'] << endl;
     }
 
-
-    string inputString;
-    getline(cin, inputString);
-
-    string encodedString;
-    for (char c : inputString) {
-        encodedString += huffmanCode[c - 'A'];
+    string input;
+    getline(cin, input);
+    string encoded;
+    for (char c : input) {
+        encoded += huffmanCode[c - 'A'];
     }
-
-
-    cout << encodedString << endl;
+    cout << encoded << endl;
 
     return 0;
 }
